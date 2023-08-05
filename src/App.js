40eba91038252c9b0b14fb4f21331a6fc841e1bc
Shri Kant Gaur <shrikantgaur05@gmail.com>
2023-08-05 17:23:01 +0530

    Added form and form handling event

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import Gallery from './components/basic-components/Gallery';
 import TodoList from './components/basic-components/TodoList';
 import TeacherList from './components/basic-components/TeacherList';
 import SculptureList from './components/basic-components/SculptureList';
+import Form from './components/basic-components/Form';
 
 function App() {
   return (
     <div className="App">
+      <Form/>
       <SculptureList/>  
       <Gallery/>
       <TodoList/>

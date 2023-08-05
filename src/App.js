6a29d3951be1ee1627a356165a8e1b9467e64d8c
Shri Kant Gaur <shrikantgaur05@gmail.com>
2023-08-05 17:08:04 +0530

    Added event and used useState() hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Gallery from './components/basic-components/Gallery';
 import TodoList from './components/basic-components/TodoList';
 import TeacherList from './components/basic-components/TeacherList';
+import SculptureList from './components/basic-components/SculptureList';
 
 function App() {
   return (
     <div className="App">
+      <SculptureList/>  
       <Gallery/>
       <TodoList/>
       <TeacherList/>

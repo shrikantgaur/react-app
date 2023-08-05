eed973eba542acf4ce69ec3e1315adbe04e32277
Shri Kant Gaur <shrikantgaur05@gmail.com>
2023-08-05 16:49:18 +0530

    Added dynamic list data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Gallery from './components/basic-components/Gallery';
 import TodoList from './components/basic-components/TodoList';
+import TeacherList from './components/basic-components/TeacherList';
 
 function App() {
   return (
     <div className="App">
       <Gallery/>
       <TodoList/>
+      <TeacherList/>
     </div>
   );
 }

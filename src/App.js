18b0b18dce1f3a74d7eb21da998f4e3b09b3fb3e
Shri Kant Gaur <shrikantgaur05@gmail.com>
2023-08-05 15:56:59 +0530

    Added todolist compoent

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Gallery from './components/basic-components/Gallery';
+import TodoList from './components/basic-components/TodoList';
 
 function App() {
   return (
     <div className="App">
       <Gallery/>
+      <TodoList/>
     </div>
   );
 }
---
+++ src/components/basic-components/TodoList.js
@@ -0,0 +1,18 @@
+function TodoList() {
+  return (
+    <>
+      <h1>Hedy Lamarr's Todos</h1>
+      <img
+        src="https://i.imgur.com/yXOvdOSs.jpg"
+        alt="Hedy Lamarr"
+        class="photo"
+      />
+      <ul>
+        <li>Invent new traffic lights</li>
+        <li>Rehearse a movie scene</li>
+        <li>Improve the spectrum technology</li>
+      </ul>
+    </>
+  );
+}
+export default TodoList;
